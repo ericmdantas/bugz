8649b854892c467f2d0dda62e45bd801a6084112
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-01 19:46:44 -0300

    .

--- cli/send.go
+++ cli/send.go
@@ -2,12 +2,20 @@ package cli
 
 import (
 	"errors"
+	"log"
 	_ "net/http"
 )
 
 func Send(opts *CliOptions) error {
 	if !opts.IsValid() {
 		return errors.New("Invalid cli params. Bug (-b) and Message (-m) are obligatory.")
+	}
+
+	c := NewContent()
+	c.Transform()
+
+	if c.IsEmpty() {
+		log.Fatalln(errors.New("Content is empty."))
 	}
 
 	return nil
